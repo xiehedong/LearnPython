c4b49151a05bcade5dbbe963a9694853e82996aa
xhd <2295138583@qq.com>
2023-09-21 15:59:57 +0800

    初始化仓库

--- test.py
+++ test.py
@@ -1,12 +1,15 @@
+# coding=utf-8 
+# Python3.X 源码文件默认使用utf-8编码，所以可以正常解析中文，无需指定 UTF-8 编码。
+
+
 #这是一行注释
 
 '''
 这是多行注释
 '''
 
-#print("hello python!")
+print("你好 python!")
 
-if False:
-    print("true")
-else:
-    print("false")
+a = 10
+b = 3
+print(a/b)
